09436ccdd4e485f6fb972473bf85eb40dd53ea25
Jamtam2 <kazmishahzeb7u@gmail.com>
2023-10-07 15:57:25 -0500

    added license get/post

--- DichoticsIncAPI/Models/AppDbContext.cs
+++ DichoticsIncAPI/Models/AppDbContext.cs
@@ -5,4 +5,5 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<Customer> Customers { get; set; }
+    public DbSet<License> Licenses { get; set; }
 }
--- DichoticsIncAPI/Models/Licenses.cs
+++ DichoticsIncAPI/Models/Licenses.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 public class License
 {
+     [Key]
+    public int LicenseID  { get; set; }
     public string ActivationCode { get; set; }
     public LicenseType LicenseType { get; set; }  // Same Enum as mentioned above
     public DateTime Expiration { get; set; }  // Assuming DateTime again
     public int ProductID { get; set; }  // Assuming it's an int based on common practices
+    public int CustomerID { get; set; }  // Assuming it's an int based on common practices
+    public int? SubscriptiontID { get; set; }  // Assuming it's an int based on common practices
+    public DateTime CreatedOnUtc { get; set; }  // Assuming DateTime again
+
 }
 
 public enum LicenseType

735e295d7ba05eae65c0d5533354e7d56c8a6909
Hunter <hjckson6@memphis.edu>
2023-12-20 23:35:27 -0600

    Fixed env in gitigone

--- RubyonRails/db/schema.rb
+++ RubyonRails/db/schema.rb
@@ -107,9 +107,6 @@ ActiveRecord::Schema.define(version: 2023_12_09_101432) do
     t.float "left_score"
     t.float "right_score"
     t.float "ear_advantage_score"
-    t.string "left_percentile"
-    t.string "right_percentile"
-    t.string "advantage_percentile"
     t.bigint "client_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -205,12 +202,7 @@ ActiveRecord::Schema.define(version: 2023_12_09_101432) do
     t.float "right_score2"
     t.float "right_score3"
     t.float "ear_advantage_score"
-    t.float "ear_advantage_score1"
-    t.float "ear_advantage_score3"
     t.string "interpretation"
-    t.string "left_percentile"
-    t.string "right_percentile"
-    t.string "advantage_percentile"
     t.bigint "client_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

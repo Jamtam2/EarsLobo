d4cab6acdb65a0291236e00baa6225a83dbba6df
Shahzeb Kazmi <kazmishahzeb7u@gmail.com>
2024-01-04 04:23:33 -0600

    changed to allow logout during mfa

--- RubyonRails/app/controllers/application_controller.rb
+++ RubyonRails/app/controllers/application_controller.rb
@@ -15,12 +15,9 @@ class ApplicationController < ActionController::Base
 
   def check_mfa
     # Bypass MFA check in development
-    # TODO: Remove this when moving to production
-    if Rails.env.development?
-      return
-    end
+    
 
-    return unless user_signed_in? && "/logout" != request.path
+    return unless user_signed_in? && "/users/sign_out" != request.path
 
   
     # Paths that should bypass MFA check

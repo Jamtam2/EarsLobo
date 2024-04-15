1601ec792d080dfe111c89151aba5d29d1162515
Shahzeb Kazmi <kazmishahzeb7u@gmail.com>
2024-04-15 13:37:41 -0500

    removed key requirement if sign-up code exists

--- RubyonRails/app/controllers/registrations_controller.rb
+++ RubyonRails/app/controllers/registrations_controller.rb
@@ -14,6 +14,8 @@ class RegistrationsController < Devise::RegistrationsController
       create_regular_user
     when 'local_moderator'
       create_local_moderator
+    # when 'location_moderator'
+    #   create_location_moderator
     else
       flash[:alert] = 'Invalid account type.'
       redirect_to new_user_registration_path and return
@@ -29,16 +31,16 @@ class RegistrationsController < Devise::RegistrationsController
     user.role = :regular_user
     local_moderator = User.find_by(role: User.roles[:local_moderator], moderator_code: params[:user][:moderator_code])
     # Validate the registration key for security purposes.
-    key = Key.find_by(activation_code: user.registration_key)
+    # key = Key.find_by(activation_code: user.verification_key)
+    # Rails.logger.debug "Params: #{params.inspect}"
 
-    if local_moderator.present? && valid_registration_key?(key)
+    if local_moderator.present?
       # The user is associated with the tenant of the local moderator whose code was entered.
       user.tenant_id = local_moderator.tenant_id
 
       # Check if user record was saved before proceeding.
       if user.save
-        
-        key.update(used: true)
+        # key.update(used: true)
         flash[:notice] = 'Regular user was successfully created.'
         sign_in(:user, user)
         redirect_to root_path, notice: 'User was successfully created set up 2FA auth.'
@@ -52,6 +54,37 @@ class RegistrationsController < Devise::RegistrationsController
       redirect_to new_user_registration_path and return
     end
   end
+
+  # def create_location_moderator
+  #   # The moderator code will be used for validation but will not be be stored under regular use.
+  #   user = User.new(sign_up_params.except(:moderator_code))
+  #   user.role = :location_moderator
+  #   local_moderator = User.find_by(role: User.roles[:local_moderator], moderator_code: params[:user][:moderator_code])
+
+  #   # Validate the registration key for security purposes.
+  #   # key = Key.find_by(activation_code: user.verification_key)
+  #   # Rails.logger.debug "Params: #{params.inspect}"
+
+  #   if local_moderator.present?
+  #     # The user is associated with the tenant of the local moderator whose code was entered.
+  #     user.tenant_id = local_moderator.tenant_id
+
+  #     # Check if user record was saved before proceeding.
+  #     if user.save
+  #       # key.update(used: true)
+  #       flash[:notice] = 'Regular user was successfully created.'
+  #       sign_in(:user, user)
+  #       redirect_to root_path, notice: 'User was successfully created set up 2FA auth.'
+  #     else
+  #       # If user creation fails, render the registration form again with error messages.
+  #       flash.now[:alert] = user.errors.full_messages.join(', ')
+  #       render :new
+  #     end
+  #   else
+  #     flash[:alert] = 'Invalid moderator code or registration key.'
+  #     redirect_to new_user_registration_path and return
+  #   end
+  # end
 
   # create_local_moderator: Creates a local moderator account. Requires a
   # valid, unused registration key. Creates a new tenant for the moderator.
@@ -68,14 +101,14 @@ class RegistrationsController < Devise::RegistrationsController
         if user.save
           # Handler for successful save actions
           key.update(used: true, email: user.email)
-          user.update(stripe_customer_id: key.customer_id,verification_key: key.activation_code)
+          user.verification_key = key.activation_code
           secret_key = ROTP::Base32.random_base32
           user.user_mfa_sessions.create!(secret_key: secret_key, activated: false)
           sign_in(:user, user)
           redirect_to root_path, notice: 'Local moderator was successfully created set up 2FA auth.'
         else
           # Handler for save failures
-          flash[:alert] = 'An internal error occurred. Try a different email, or reinput your license key.'
+          flash[:alert] = 'An internal error occurred.'
           Rails.logger.info("DEBUG: Failed to save user: #{user.errors.full_messages}")
           redirect_to new_user_registration_path and return
         end
@@ -90,17 +123,18 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def valid_registration_key?(key)
+    # puts "Key validity check: #{key.inspect}"
     key.present? && !key.used && (key.expiration.nil? || key.expiration > Time.current)
   end
-
-  
 
   private
 
   def sign_up_params
     allowed_params = [:fname, :lname, :email, :password, :password_confirmation, :verification_key]
     allowed_params << :moderator_code if params[:account_type] == 'regular_user'
+    Rails.logger.debug "Account Type: #{params[:account_type]}"
     params.require(:user).permit(allowed_params)
   end
+
 
 end
---
+++ d
@@ -0,0 +1,71 @@
+[33mcommit 4cc486bf43ee9855c1f2644ecb949002592c2d7d[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Jamtam2 <70536274+Jamtam2@users.noreply.github.com>
+Date:   Tue Jan 9 23:04:12 2024 -0600
+
+    Update seeds.rb
+
+[33mcommit 7481ae55dc1d188ffb82b3d077fffc7ac7bebcd3[m
+Author: Jamtam2 <70536274+Jamtam2@users.noreply.github.com>
+Date:   Tue Jan 9 22:35:57 2024 -0600
+
+    Update application.html.erb
+
+[33mcommit c405fc63cb0f060c5d2394310ae772c7089cda2a[m
+Author: Jamtam2 <70536274+Jamtam2@users.noreply.github.com>
+Date:   Tue Jan 9 22:07:57 2024 -0600
+
+    Delete app/controllers/users/users_controller.rb
+
+[33mcommit 3f03c748b88dad3883c713e1fd54bb7df6a0595e[m
+Author: Jamtam2 <70536274+Jamtam2@users.noreply.github.com>
+Date:   Tue Jan 9 22:02:33 2024 -0600
+
+    Update users_controller.rb
+
+[33mcommit 7caa060236df54595a3bdec88496997ba031314e[m
+Author: Jamtam2 <70536274+Jamtam2@users.noreply.github.com>
+Date:   Tue Jan 9 21:47:32 2024 -0600
+
+    Update babel.config.js
+
+[33mcommit 3be0289b0daae3c9e8a3e39dd2af3f9adc4497b5[m
+Author: Jamtam2 <70536274+Jamtam2@users.noreply.github.com>
+Date:   Tue Jan 9 21:32:13 2024 -0600
+
+    Update tablesScroll.css
+
+[33mcommit 4bb67323b5fdf9e2bed5f25717a114e091c2d69a[m
+Author: Jamtam2 <70536274+Jamtam2@users.noreply.github.com>
+Date:   Tue Jan 9 21:27:26 2024 -0600
+
+    Update buttons.css
+
+[33mcommit 17d5abc5d713c20085199a2a71f467c3ed2b6077[m
+Author: Jamtam2 <70536274+Jamtam2@users.noreply.github.com>
+Date:   Tue Jan 9 21:23:09 2024 -0600
+
+    Update buttons.css
+
+[33mcommit b5a08f76cdd39dbd2061dfab35cb6c88ebf8a61d[m
+Author: Jamtam2 <70536274+Jamtam2@users.noreply.github.com>
+Date:   Tue Jan 9 21:16:34 2024 -0600
+
+    Update buttons.css
+
+[33mcommit 2a80c23443f2642341b277891ca76241474d5035[m
+Author: Jamtam2 <70536274+Jamtam2@users.noreply.github.com>
+Date:   Tue Jan 9 21:08:12 2024 -0600
+
+    Update background.css
+
+[33mcommit 1972c4ad6cdad2c27357271c9f3f130ad5ce5990[m
+Author: Shahzeb Kazmi <kazmishahzeb7u@gmail.com>
+Date:   Tue Jan 9 18:10:52 2024 -0600
+
+    added procfile for heroku deployment
+
+[33mcommit 9fc9f84195c9baede6c59ad75d88f6eeb60dbd3b[m
+Author: Shahzeb Kazmi <kazmishahzeb7u@gmail.com>
+Date:   Tue Jan 9 18:01:47 2024 -0600
+
+    made ruby on rails the root repository for deployment
